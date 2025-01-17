8a4bdc211675fd4734c6a15338746250bfd47d46
Marcin Lach <marcin.lach.90@gmail.com>
2025-01-17 19:43:09 +0100

    Mark WorkItem as complete or incomplete (#3)
    * Marking WorkItem as complete or incomplete
    
    * Separate API models for separate operations
    
    * Endpoints to mark work item as complete or incomplete

---
+++ src/Modules/WorkItems/yatt.WorkItems.Api/Models/AddWorkItemRequestModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace yatt.WorkItems.Api.Models;
+
+public record AddWorkItemRequestModel
+{
+    [Description("Identifier of the work item")]
+    public required Guid Id { get; init; }
+    [Description("Brief description of the work item")]
+    public required string Title { get; init; }
+    [Description("Detailed description of the work item")]
+    public string? Details { get; init; }
+}
+
+public static class AddWorkItemMapping
+{
+    public static WorkItem ToWorkItem(this AddWorkItemRequestModel responseModel)
+    {
+        return new WorkItem
+        {
+            Id = responseModel.Id,
+            Title = responseModel.Title,
+            Details = responseModel.Details,
+        };
+    }
+}

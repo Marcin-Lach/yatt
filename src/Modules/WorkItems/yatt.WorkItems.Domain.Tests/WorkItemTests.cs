3cf42add218a44f7545209e73ba7810b6ccd1fb5
Marcin Lach <marcin.lach.90@gmail.com>
2025-01-17 18:58:07 +0100

    Marking WorkItem as complete or incomplete

---
+++ src/Modules/WorkItems/yatt.WorkItems.Domain.Tests/WorkItemTests.cs
@@ -0,0 +1,64 @@
+﻿using yatt.WorkItems.Api.Models;
+
+namespace yatt.WorkItems.Domain.Tests;
+
+public class WorkItemTests
+{
+    [Test]
+    public async Task Given_IncompletedWorkItem_When_MarkingAsComplete_Should_BeComplete()
+    {
+        var wi = CreateIncompletedWorkItem();
+
+        wi.MarkAsComplete();
+
+        await Assert.That(wi.IsCompleted).IsTrue();
+    }
+
+    [Test]
+    public async Task Given_CompletedWorkItem_When_MarkingAsComplete_Should_BeComplete()
+    {
+        var wi = CreateCompletedWorkItem();
+
+        wi.MarkAsComplete();
+
+        await Assert.That(wi.IsCompleted).IsTrue();
+    }
+
+    [Test]
+    public async Task Given_CompletedWorkItem_When_MarkingAsIncomplete_Should_BeIncomplete()
+    {
+        var wi = CreateCompletedWorkItem();
+
+        wi.MarkAsIncomplete();
+
+        await Assert.That(wi.IsCompleted).IsFalse();
+    }
+    
+    [Test]
+    public async Task Given_IncompletedWorkItem_When_MarkingAsIncomplete_Should_BeIncomplete()
+    {
+        var wi = CreateIncompletedWorkItem();
+
+        wi.MarkAsIncomplete();
+
+        await Assert.That(wi.IsCompleted).IsFalse();
+    }
+
+    private static WorkItem CreateIncompletedWorkItem()
+    {
+        var wi = new WorkItem()
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test",
+            Details = "dets"
+        };
+        return wi;
+    }
+
+    private static WorkItem CreateCompletedWorkItem()
+    {
+        var wi = CreateIncompletedWorkItem();
+        wi.MarkAsComplete();
+        return wi;
+    }
+}
--- src/Modules/WorkItems/yatt.WorkItems.Domain/Models/WorkItem.cs
+++ src/Modules/WorkItems/yatt.WorkItems.Domain/Models/WorkItem.cs
@@ -5,4 +5,16 @@ public class WorkItem
     public required Guid Id { get; init; }
     public required string Title { get; init; }
     public string? Details { get; init; }
+
+    public bool IsCompleted { get; protected set; } = false;
+
+    public void MarkAsComplete()
+    {
+        IsCompleted = true;
+    }
+
+    public void MarkAsIncomplete()
+    {
+        IsCompleted = false;
+    }
 }
